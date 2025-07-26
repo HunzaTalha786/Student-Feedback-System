ec69e958f985e7181f76aa23fc37ed6ccf4c8915
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-07-27 03:30:16 +0500

    Done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -15,11 +15,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://student-feedback-system-backend.vercel.app/", // ✅ Replace with your actual backend URL
-  withCredentials: true, // ✅ Only needed if using cookies (safe to include)
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  
 });
 
-// Add JWT token to headers if exists
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
@@ -27,3 +26,4 @@ API.interceptors.request.use((req) => {
 });
 
 export default API;
+
