862c4ae41cbb084a26c68f5397edc0f4de7b078f
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-07-27 01:37:11 +0500

    Done

--- backend/index.js
+++ backend/index.js
@@ -2,33 +2,33 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import serverless from "serverless-http";
 
-import feedbackRoutes from "./routes/feedbackRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js";
+import feedbackRoutes from "../routes/feedbackRoutes.js";
+import adminRoutes from "../routes/adminRoutes.js";
 
 dotenv.config();
 
 const app = express();
-app.use(cors(
-  {
-    origin:process.env.FRONT_END_URL,
-  }
-));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/feedback", feedbackRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Student Feedback API is running");
+app.get("/api", (req, res) => {
+  res.send("Student Feedback API is running on Vercel 🎉");
 });
 
+// MongoDB connection (safe in serverless)
+let isConnected = false;
+async function connectDB() {
+  if (!isConnected) {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("✅ MongoDB Connected");
+  }
+}
+await connectDB();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on http://localhost:${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.log(err));
+export default serverless(app);
