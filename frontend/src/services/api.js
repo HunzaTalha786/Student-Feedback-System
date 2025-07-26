c284fe506221fb4dfab0840bdb487546d79a18f8
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-07-27 00:52:34 +0500

    Done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
-
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+})
 // Add token to headers
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
