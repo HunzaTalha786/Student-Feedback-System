d00ab06f8c56f27d89a22288a46316ce41a86052
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-07-27 02:36:01 +0500

    Done

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import feedbackRoutes from "./routes/feedbackRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/feedback", feedbackRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.get("/", (req, res) => {
+  res.send("✅ Student Feedback API is running");
+});
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on http://localhost:${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
