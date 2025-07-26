5b06b526bf36583559aa75832867404f49f26261
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-07-27 02:15:42 +0500

    Done

--- backend/api/index.js
+++ backend/api/index.js
@@ -13,21 +13,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/feedback", feedbackRoutes);
-app.use("/admin", adminRoutes);
+app.use("api/feedback", feedbackRoutes);
+app.use("api/admin", adminRoutes);
 
-app.get("/", (req, res) => {
+app.get("api/", (req, res) => {
   res.send("Student Feedback API is running");
 });
 
-let isConnected = false;
-async function connectDB() {
-  if (!isConnected) {
-    await mongoose.connect(process.env.MONGO_URI);
-    isConnected = true;
-    console.log("✅ MongoDB Connected");
-  }
-}
-await connectDB();
 
-export default serverless(app);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on http://localhost:${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
+
+  export default serverless(app);
